d4872273f7e21614f3fdcb81abcecdabb1ff8257
Cory Aitchison <cory.aitchison@sydney.edu.au>
2023-08-21 11:50:26 +1000

    Implement latex

--- lua/compiler/languages/tex.lua
+++ lua/compiler/languages/tex.lua
@@ -33,103 +33,43 @@ function M.action(selected_option)
     local task = overseer.new_task({
       name = "- LaTeX compiler",
       strategy = { "orchestrator",
-        tasks = {{ "shell", name = "- Build program → " .. entry_point,
-          cmd = "rm -f " .. output ..                                                -- clean
-                " && mkdir -p " .. output_dir ..                                     -- mkdir
-                " && gcc " .. files .. " -o " .. output .. " " .. arguments ..       -- compile
-                " && echo " .. entry_point ..                                        -- echo
+        tasks = {{ "shell", name = "- Build & open project → " .. entry_point,
+          cmd = "latexmk -pdf -interaction=nonstopmode -synctex=1 -pv " .. entry_point ..
                 " && echo '" .. final_message .. "'"
         },},},})
     task:start()
     vim.cmd("OverseerOpen")
   elseif selected_option == "option3" then
     local task = overseer.new_task({
-      name = "- C compiler",
+      name = "- LaTeX compiler",
       strategy = { "orchestrator",
-        tasks = {{ "shell", name = "- Run program → " .. entry_point,
-          cmd = output ..                                                    -- run
-                " && echo " .. output ..                                     -- echo
+        tasks = {{ "shell", name = "- Build project → " .. entry_point,
+          cmd = "latexmk -pdf -interaction=nonstopmode -synctex=1 " .. entry_point ..
                 " && echo '" .. final_message .. "'"
         },},},})
     task:start()
     vim.cmd("OverseerOpen")
   elseif selected_option == "option4" then
-    local entry_points
-    local tasks = {}
-    local task
-
-    -- if .solution file exists in working dir
-    local solution_file = utils.get_solution_file()
-    if solution_file then
-      local config = utils.parse_solution_file(solution_file)
-      local executable
-
-      for entry, variables in pairs(config) do
-        if variables.executable then
-          executable = utils.os_path(variables.executable)
-          goto continue
-        end
-        entry_point = utils.os_path(variables.entry_point)
-        files = utils.find_files_to_compile(entry_point, "*.c")
-        output = utils.os_path(variables.output)
-        output_dir = utils.os_path(output:match("^(.-[/\\])[^/\\]*$"))
-        arguments = variables.arguments or arguments -- optional
-        task = { "shell", name = "- Build program → " .. entry_point,
-          cmd = "rm -f " .. output ..                                                -- clean
-                " && mkdir -p " .. output_dir ..                                     -- mkdir
-                " && gcc " .. files .. " -o " .. output .. " " .. arguments ..       -- compile
-                " && echo " .. entry_point ..                                        -- echo
+    local task = overseer.new_task({
+      name = "- LaTeX compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Clean → " .. entry_point,
+          cmd = "latexmk -c" ..
                 " && echo '" .. final_message .. "'"
-        }
-        table.insert(tasks, task) -- store all the tasks we've created
-        ::continue::
-      end
-
-      if executable then
-        task = { "shell", name = "- Run program → " .. executable,
-          cmd = executable ..                                                -- run
-                " && echo " .. executable ..                                 -- echo
-                " && echo '" .. final_message .. "'"
-        }
-      else
-        task = {}
-      end
-
-      task = overseer.new_task({
-        name = "- C compiler", strategy = { "orchestrator",
-          tasks = {
-            tasks, -- Build all the programs in the solution in parallel
-            task   -- Then run the solution executable
-          }}})
-      task:start()
-      vim.cmd("OverseerOpen")
-
-    else -- If no .solution file
-      -- Create a list of all entry point files in the working directory
-      entry_points = utils.find_files(vim.fn.getcwd(), "main.c")
-
-      for _, entry_point in ipairs(entry_points) do
-        entry_point = utils.os_path(entry_point)
-        files = utils.find_files_to_compile(entry_point, "*.c")
-        output_dir = utils.os_path(entry_point:match("^(.-[/\\])[^/\\]*$") .. "bin")  -- entry_point/bin
-        output = utils.os_path(output_dir .. "/program")                              -- entry_point/bin/program
-        task = { "shell", name = "- Build program → " .. entry_point,
-          cmd = "rm -f " .. output ..                                                -- clean
-                " && mkdir -p " .. output_dir ..                                     -- mkdir
-                " && gcc " .. files .. " -o " .. output .. " " .. arguments ..       -- compile
-                " && echo " .. entry_point ..                                        -- echo
-                " && echo '" .. final_message .. "'"
-        }
-        table.insert(tasks, task) -- store all the tasks we've created
-      end
-
-      task = overseer.new_task({ -- run all tasks we've created in parallel
-        name = "- C compiler", strategy = { "orchestrator", tasks = tasks }
-      })
-      task:start()
-      vim.cmd("OverseerOpen")
-    end
+        },},},})
+    task:start()
+    vim.cmd("OverseerOpen")
   elseif selected_option == "option5" then
+    local task = overseer.new_task({
+      name = "- LaTeX compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Deep Clean → " .. entry_point,
+          cmd = "latexmk -C" ..
+                " && echo '" .. final_message .. "'"
+        },},},})
+    task:start()
+    vim.cmd("OverseerOpen")
+  elseif selected_option == "option6" then
     require("compiler.languages.make").run_makefile()                        -- run
   end
 end
